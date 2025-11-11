8889699f241ad87642d46dfa1c4a5b25146c3a8a
pipeta <urienfelipe@gmail.com>
2025-11-11 20:29:13 -0300

    Ejercicio 3 Programacion Dinamica

---
+++ Ejercicio3ProgramacionDinamica.py
@@ -0,0 +1,25 @@
+def knapsack_01(valor, peso, capacidad):
+    n = len(valor)
+    dp = [0] * (capacidad + 1)
+    for i in range(n):
+        wi, vi = peso[i], valor[i]
+        # recorrer capacidades DE MAYOR A MENOR
+        for c in range(capacidad, wi - 1, -1):
+            dp[c] = max(dp[c], vi + dp[c - wi])
+    return dp[capacidad]
+
+
+valor = [10, 14, 16]
+peso = [2, 3, 4]
+capacidad = 7
+print("Valor maximo obtenido en Ejemplo 1:" , knapsack_01(valor, peso, capacidad))
+
+valor = [20, 5, 10, 40, 15, 25]
+peso = [1, 2, 3, 8, 7, 4]
+capacidad = 10
+print("Valor maximo obtenido en Ejemplo 2:" , knapsack_01(valor, peso, capacidad))
+
+valor = [5, 8, 14, 6, 12]
+peso = [3, 5, 6, 2, 4]
+capacidad = 10
+print("Valor maximo obtenido en Ejemplo 3:" , knapsack_01(valor, peso, capacidad))
